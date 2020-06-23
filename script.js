c34e6f7644542d28a8879aa9dc400adb47e8a6c4
dfprismarine <xmattisbad@gmail.com>
2020-06-24 00:10:39 +0200

    Themes

--- script.js
+++ script.js
@@ -6,66 +6,69 @@
     theme: 'dracula'
   });
 */
-  var myCodeMirror = CodeMirror(document.body, {
+var myCodeMirror = CodeMirror(document.body, {
     lineNumbers: true,
     tabSize: 2,
     value: 'new codeLine("event", "Join")\n.playerAction("SendMessage", "The Website Works.")\n.build();',
     mode: 'javascript',
     theme: 'dracula'
-  });
-  
+});
 
-  function showCode()
-  {
-      var text = myCodeMirror.getValue();
-      console.log("\n" + text + "\n ");
-  }
+function changeTheme(theme) {
+    console.log(theme)
+    myCodeMirror.setOption("theme", theme);
+}
 
-  showCode();
+function showCode() {
+    var text = myCodeMirror.getValue();
+    console.log("\n" + text + "\n ");
+}
 
-  function json() {
+showCode();
+
+function json() {
     try {
-      var a = eval(myCodeMirror.getValue());
-      document.getElementById("codebox").value = a.json;
+        var a = eval(myCodeMirror.getValue());
+        document.getElementById("codebox").value = a.json;
     } catch (e) {
-      alert(e); 
+        alert(e);
     }
-  }
+}
 
-  function encode() {
+function encode() {
     try {
-      var a = eval(myCodeMirror.getValue());
-      document.getElementById("codebox").value = a.encoded;
+        var a = eval(myCodeMirror.getValue());
+        document.getElementById("codebox").value = a.encoded;
     } catch (e) {
-      alert(e); 
+        alert(e);
     }
-  }
+}
 
 
-  function giveCommand() {
-      try {
+function giveCommand() {
+    try {
         var a = eval(myCodeMirror.getValue());
         document.getElementById("codebox").value = a.give;
-      } catch (e) {
-        alert(e); 
-      }
+    } catch (e) {
+        alert(e);
     }
+}
 
-  function copy() {
+function copy() {
     alert("coming soon.");
-  }
+}
 
-  function sharecode() {
+function sharecode() {
     document.getElementById("codebox").value = "https://dfprismarine.github.io/?code=" + btoa(myCodeMirror.getValue());
-  }
+}
 
 var urlParams = new URLSearchParams(window.location.search);
 var customCode = urlParams.get("code");
 if (customCode != null) {
-  try {
-    myCodeMirror.setValue(atob(customCode))
-  } catch (e) {
-    alert("There was an error while decrypting your code.")
-    console.log(e);
-  }
+    try {
+        myCodeMirror.setValue(atob(customCode))
+    } catch (e) {
+        alert("There was an error while decrypting your code.")
+        console.log(e);
+    }
 }
